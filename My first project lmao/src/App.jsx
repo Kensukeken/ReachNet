10eeef1025fc5f77b76fec4d2f7e88d6229a1876
Kensukeken <hiaxsaleh@gmail.com>
2024-02-25 20:26:51 -0500

    Update

---
+++ My first project lmao/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from './Components/home'
+
+const App = () => {
+  return (
+    <>
+    <h1>Welcome to my website</h1>
+    <div>
+      <h1>Hello</h1>
+      <Home/>
+      <Home/>
+      <Home/>
+      <Home/>
+      <Home/>
+    </div>
+    </>
+  )
+}
+
+export default App
